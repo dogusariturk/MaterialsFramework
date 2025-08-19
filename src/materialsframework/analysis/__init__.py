c6e368a753e2d82c3e39a962c16154c40eda9810
dogusariturk <dogu.sariturk@gmail.com>
2025-08-19 08:23:50 -0500

    Refactor SurfaceAnalyzer

--- src/materialsframework/analysis/__init__.py
+++ src/materialsframework/analysis/__init__.py
@@ -1,4 +1,5 @@
-""" This package contains the analysis tools for MaterialsFramework. """
+"""This package contains the analysis tools for MaterialsFramework."""
+
 from .annni import ANNNIStackingFaultAnalyzer
 from .bain import BainPathAnalyzer
 from .cubic_elastic_constants import CubicElasticConstantsAnalyzer
@@ -6,6 +7,7 @@ from .elastic_constants import ElasticConstantsAnalyzer
 from .eos import EOSAnalyzer
 from .phono3py import Phono3pyAnalyzer
 from .phonopy import PhonopyAnalyzer
+from .surface import SurfaceAnalyzer
 
 __author__ = "Doguhan Sariturk"
 __email__ = "dogu.sariturk@gmail.com"
